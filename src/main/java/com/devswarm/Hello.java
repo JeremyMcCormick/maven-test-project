70c8fab4030161f29792af9a7aa8c24894d20e3e
Jeremy McCormick <jermccormick@gmail.com>
2019-08-18 10:09:33 +0000

    Add simple test.

--- src/main/java/com/devswarm/Hello.java
+++ src/main/java/com/devswarm/Hello.java
@@ -1,8 +1,11 @@
 package com.devswarm;
 
-class Hello {
+import java.util.Arrays;
+
+public class Hello {
     
     public static void main(String args[]) {
-        System.out.println("Hello Maven Test Project!");    
+        System.out.println("Hello Maven Test Project!");
+        System.out.println("You said: " + String.join(" ", args));
     }
 }
---
+++ src/test/java/com/devswarm/HelloTest.java
@@ -0,0 +1,9 @@
+package com.devswarm;
+
+public class HelloTest {
+    
+    public void testHello() {
+        String args[] = {"foo", "bar", "baz"};
+        Hello.main(args);
+    }
+}
