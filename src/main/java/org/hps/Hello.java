8a60ec96e066179d476be8e00e47b32cae46a27a
Jeremy McCormick <jermccormick@gmail.com>
2023-03-28 15:07:46 -0500

    move hello java

---
+++ src/main/java/org/hps/Hello.java
@@ -0,0 +1,11 @@
+package com.devswarm;
+
+import java.util.Arrays;
+
+public class Hello {
+    
+    public static void main(String args[]) {
+        System.out.println("Hello Maven Test Project!");
+        System.out.println("You said: " + String.join(" ", args));
+    }
+}
