aee9c409b9cd89cf6ff67cd8d1abc95f48c77a8e
Jeremy McCormick <jermccormick@gmail.com>
2023-03-28 20:56:53 -0500

    small changes to rel script

--- perform_release.sh
+++ perform_release.sh
@@ -12,7 +12,8 @@ echo ">>>> Switching to master branch"
 
 git checkout master && git pull origin master
 
-[[ $(git diff origin/master) ]] && echo "ERROR: Local master is different from origin!" && git --no-pager diff origin/master && exit 1
+[[ $(git diff origin/master) ]] && git --no-pager diff origin/master && \
+  echo "ERROR: Local master is different from origin/master! (see above)" && exit 1
 
 #echo ">>>> Checking for clean master"
 #[[ ! -z "$(git status --porcelain)" ]] && echo "ERROR: Working copy of master is not clean." && exit 1
@@ -24,3 +25,5 @@ echo ">>> Pushing tag to github"
 git push origin $relname
 
 echo ">>> Done!"
+
+echo "Check here for release workflow: https://github.com/JeremyMcCormick/maven-test-project/actions"
