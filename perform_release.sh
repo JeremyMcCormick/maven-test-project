faadd8c4859f8154b502c9cf6eca701dadd831b8
Jeremy McCormick <jermccormick@gmail.com>
2023-03-28 20:45:37 -0500

    update rel script

--- perform_release.sh
+++ perform_release.sh
@@ -1,18 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
-relname=$1
-[ -z "$relname" ] && echo "ERROR: No release name was given as argument." && exit 1
+[ -z "$1" ] && echo "ERROR: No release name was given as argument." && exit 1
 
-echo "Making release: $relname"
+relname=releases/$1
 
-echo "Checking out master branch ..."
-#git checkout master && git pull origin master
-git stash; git fetch origin; git reset --hard origin/master
+echo ">>>> Making release: $relname"
 
-echo "Creating local tag ..."
-git tag -a releases/$relname
+if [[ $(git tag -l "$relname") ]]; then echo "ERROR: Tag $relname already exists." && exit 1; fi
 
-echo "Pushing tag to github ..."
-git push origin releases/$relname
+echo ">>>> Switching to master branch"
 
-echo "Done!"
+git checkout master
+
+#echo ">>> Checking out master branch ..."
+#git stash; git fetch origin; git reset --hard origin/master
+
+echo ">>>> Pulling master from origin"
+git pull origin master
+
+echo ">>>> Checking for clean master"
+if [[ ! -z "$(git status --porcelain)" ]]; then echo "ERROR: Working copy of master is not clean." && exit 1; fi
+
+echo ">>> Creating local tag"
+git tag -a $relname
+
+echo ">>> Pushing tag to github"
+git push origin $relname
+
+echo ">>> Done!"
