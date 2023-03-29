f009e4901c79ef1dd0e7aaa51e7f650fbc5694df
Jeremy McCormick <jermccormick@gmail.com>
2023-03-28 22:19:21 -0500

    remove garbage template stuff

--- gen_site.sh
+++
@@ -1,7 +0,0 @@
-mvn archetype:generate \
-    -DgroupId=org.hps \
-    -DartifactId=maven-test-project \
-    -DarchetypeGroupId=org.apache.maven.archetypes \
-    -DarchetypeArtifactId=maven-archetype-site \
-    -DarchetypeVersion=1.4 \
-    -DinteractiveMode=false
