df521dd8506e55fefd09aea70fe07c45f4f9eac1
Jeremy McCormick <jermccormick@gmail.com>
2023-03-28 22:11:18 -0500

    add script for generating site archtype

---
+++ gen_site.sh
@@ -0,0 +1,7 @@
+mvn archetype:generate \
+    -DgroupId=org.hps \
+    -DartifactId=maven-test-project \
+    -DarchetypeGroupId=org.apache.maven.archetypes \
+    -DarchetypeArtifactId=maven-archetype-site \
+    -DarchetypeVersion=1.4 \
+    -DinteractiveMode=false
